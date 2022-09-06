60641f5635e27c4c01426cb32b3bfd84a6c565ca
马文卿 <529837505@qq.com>
2022-09-06 20:35:24 +0800

    commit

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,4 +1,4 @@
-const { exec } = require('../db/mysql');
+const { exec, escape } = require('../db/mysql');
 
 /**
  * user login
@@ -7,7 +7,9 @@ const { exec } = require('../db/mysql');
  * @returns 
  */
 const login = (username, password) => {
-    const sql = `select username, realname from users where username='${username}' and password='${password}'`
+    username = escape(username)
+    password = escape(password)
+    const sql = `select username, realname from users where username=${username} and password=${password}`
     return exec(sql).then(res => {
         return res[0] || {};
     })
