b0548b372008578373ec34e09286be1bdbeb6a7c
马文卿 <529837505@qq.com>
2022-09-20 20:24:41 +0800

    commit

--- blog-express/app.js
+++ blog-express/app.js
@@ -18,7 +18,9 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-app.use(logger('dev')); 
+app.use(logger('dev'), {
+  stream: process.stdout
+}); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
